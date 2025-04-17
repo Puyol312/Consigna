83b54c2455c716af396ed88509af7d1137f2e4fb
cpl010 <caiopuyolleguiza@gmail.com>
2025-04-17 16:05:12 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const pelis = require(__dirname + '/pelis.js');
+function main(){
+    const comandos = process.argv.slice(2);
+    if (comandos.length % 2 !== 0){
+        throw "Mal subidos los datos, falta un parametro";
+    }
+    const data = pelis.conseguirDatos();
+    for(let i = 0; i<comandos.length; i+=2){
+        switch(comandos[i]){
+            case ('--sort'):
+                console.log(`Ordenado por ${comandos[(i+1)]}:\n`,pelis.sort(comandos[(i+1)], data));
+                break;
+            case ('--search'):
+                console.log(`Buscando por ${comandos[(i+1)]}:\n`,pelis.search(comandos[(i+1)], data));
+                break;
+            case ('--tag'):
+                console.log(`Buscando por ${comandos[(i+1)]}:\n`,pelis.tag(comandos[(i+1)], data));
+                break;
+            default:
+                console.log(`Comando no encontrado: ${comandos[i]}`);
+                break;
+        }
+    }
+    console.log('Fin');
+}
+main();
---
+++ pelis.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+//Funcion que consigue los datos.
+function conseguirDatos(){
+    const archivoTexto = (fs.readFileSync(__dirname +"/pelis.json")).toString();
+    const objeto = JSON.parse(archivoTexto);
+    return objeto;
+}
+//Funcion que recibe un paremtro y un array de objetos, devuelve un nuevo array ordenado segun el 
+// parametro, alfabeticamente o de mayor a menor.
+function sort(argumentoN, datos){
+    const nuevoDato = [];
+    const argumento = argumentoN.toLowerCase();
+    if (argumento == 'titulo'){
+        nuevoDato = [...datos].sort((a, b) => a.title.localeCompare(b.title));
+    }else if(argumento =='rating'){
+        nuevoDato = [...datos].sort((a, b) => b.rating - a.rating);
+    }else if(argumento == 'tags'){
+        nuevoDato = [...datos].sort((a, b) => b.tags.length - a.tags.length);
+    }else{
+        console.log(`Campo no encontrado: ${argumento}`);
+    }
+    return nuevoDato;
+}
+//devolver las películas que tengan la palabra "parametro" en su titulo. No es case sensitive
+function search(parametro, datos){
+    return datos.filter(obj => obj.title.toLowerCase().includes(parametro.toLowerCase()));
+}
+
+//Devuelve aquellas peliculas en tengan en su campo tag el 'parametro', sin importar como lo 
+//haya escrito el usuario.
+function tag(parametro, datos){
+    return datos.filter(obj => obj.tags?.includes(parametro.toLowerCase()));
+}
+
+module.exports = {conseguirDatos, sort, search, tag};
